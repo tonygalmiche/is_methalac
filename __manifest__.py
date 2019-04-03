6e1ebdca1b23701eeb793b7dc221ee9380b216da
Tony Galmiche <contact@infosaone.com>
2019-04-03 10:57:32 +0200

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name'     : 'InfoSaône - Module Odoo 12 pour Methalac',
+    'version'  : '0.1',
+    'author'   : 'InfoSaône',
+    'category' : 'InfoSaône',
+    'description': """
+InfoSaône - Module Odoo 12 pour Methalac
+===================================================
+""",
+    'maintainer' : 'InfoSaône',
+    'website'    : 'http://www.infosaone.com',
+    'depends'    : [
+        'base',
+        'document',
+        'product',
+        'purchase',
+    ],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': [
+    ],
+}
+
