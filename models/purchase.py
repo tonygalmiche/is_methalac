86efbd66661f311a69ff3ff97b8d3c11bd3120cf
Tony Galmiche <tony.galmiche@infosaone.com>
2019-07-24 12:00:14 +0200

    modifs commande achat

--- models/purchase.py
+++ models/purchase.py
@@ -2,9 +2,10 @@
 from odoo import api, fields, models, _
 
 
-
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     is_adresse_livraison_id = fields.Many2one('res.partner', "Adresse de livraison")
     is_client_final_id      = fields.Many2one('res.partner', "Client final")
+    is_contact_client_id    = fields.Many2one('res.partner', "Contact Client")
+    is_contact_monteur_id   = fields.Many2one('res.partner', "Contact Monteur")
